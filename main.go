ece095cd75810987a8dde9f0d769cb7f6f3c2364
Preethi Vuchuru <PreethiVuchuru@Preethis-MacBook-Pro.local>
2022-05-19 22:01:23 -0700

    Made changes to the db files

--- main.go
+++ main.go
@@ -1,25 +1,21 @@
 package main
 
 import (
-    "gopkg.in/ini.v1"
-    "io/ioutil"
-    "fmt"
-    "mini/minichain" 
-    _ "github.com/go-kivik/couchdb/v3"
+	"fmt"
+	"io/ioutil"
+	"mini/minichain"
+
+	_ "github.com/go-kivik/couchdb/v3"
+	"gopkg.in/ini.v1"
 )
 
 func main() {
-    cfg, _ := ini.Load("my.ini")
-    bcAssetID := cfg.Section("").Key("BCAssetId").String()
-    bcAssetType := cfg.Section("").Key("BCAssetType").String()
-    BCAssetJsonSchemaFile := cfg.Section("").Key("BCAssetJsonSchemaFile").String()
-    BCAssetIdGet := cfg.Section("").Key("BCAssetIdGet").String()
-    bcKey := bcAssetType + "~" + bcAssetID
-    file, _ := ioutil.ReadFile(BCAssetJsonSchemaFile)
-    fmt.Println(minichain.PutState(bcKey, string(file)))
-    fmt.Println(minichain.GetState(BCAssetIdGet))
+	cfg, _ := ini.Load("my.ini")
+	bcAssetID := cfg.Section("").Key("BCAssetId").String()
+	bcAssetType := cfg.Section("").Key("BCAssetType").String()
+	BCAssetJsonSchemaFile := cfg.Section("").Key("BCAssetJsonSchemaFile").String()
+	bcKey := "Gateway~bc~" + bcAssetType + "~" + bcAssetID
+	file, _ := ioutil.ReadFile(BCAssetJsonSchemaFile)
+	fmt.Println(minichain.PutState(bcKey, file))
+	fmt.Println(string(minichain.GetState((bcKey))))
 }
-
-
-
-
--- minichain/state.go
+++ minichain/state.go
@@ -1,43 +1,41 @@
 package minichain
 
 import (
-    "gopkg.in/ini.v1"
-    "io"
-    "context"
-    kivik "github.com/go-kivik/kivik/v3"
-    _ "github.com/go-kivik/couchdb/v3" 
+	"context"
+	"io"
+
+	_ "github.com/go-kivik/couchdb/v3"
+	kivik "github.com/go-kivik/kivik/v3"
+	"gopkg.in/ini.v1"
 )
 
-func GetConnection()(*kivik.DB){
-    cfg, _ := ini.Load("my.ini")
-    connectionURL := cfg.Section("").Key("ConnectionURL").String()
-    databaseName := cfg.Section("").Key("DatabaseName").String()
-    client, err := kivik.New("couch", connectionURL)
-    if err != nil {
-        panic(err)
-    }
-    db := client.DB(context.TODO(), databaseName)
-    return db
+func GetConnection() (*kivik.Client, *kivik.DB) {
+	cfg, _ := ini.Load("my.ini")
+	connectionURL := cfg.Section("").Key("ConnectionURL").String()
+	databaseName := cfg.Section("").Key("DatabaseName").String()
+	client, err := kivik.New("couch", connectionURL)
+	if err != nil {
+		panic(err)
+	}
+	db := client.DB(context.TODO(), databaseName)
+	return client, db
 }
 
-func PutState(key string, value string)(string){
-    db := GetConnection()
-    rev, err := db.Put(context.TODO(), key, value)
-    if err != nil {
-        panic(err)
-    }
-    return rev
+var client, db = GetConnection()
+
+func PutState(key string, value []byte) string {
+	rev, err := db.Put(context.TODO(), key, string(value))
+	if err != nil {
+		panic(err)
+	}
+	return rev
 }
 
-func GetState(key string)(string){
-    db := GetConnection()
-    row := db.Get(context.TODO(), key) // Returns the content length and an io.ReadCloser(Body) to access the raw JSON content
-    if row.ContentLength <= 0 {
-        return "Invalid Key"
-    }    
-    bytes, err := io.ReadAll(row.Body) // row.Body is a io.ReadCloser which is converted to bytes using io.ReadAll method
-    if err != nil {
-        panic(err)
-    }
-    return string(bytes)
+func GetState(key string) []byte {
+	row := db.Get(context.TODO(), key) // Returns the content length and an io.ReadCloser(Body) to access the raw JSON content
+	bytes, err := io.ReadAll(row.Body) // row.Body is a io.ReadCloser which is converted to bytes using io.ReadAll method
+	if err != nil {
+		panic(err)
+	}
+	return bytes
 }
